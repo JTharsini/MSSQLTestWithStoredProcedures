92019650e8f4d5ba827d8a5d8ca064229dc4c56e
JJeyatharsini <jeyaganeshan.jeyatharsini@cambio.lk>
2023-10-01 03:38:28 +0530

    fixed

--- TableSchema.sql
+++ TableSchema.sql
@@ -15,21 +15,57 @@ CREATE TABLE [dbo].[sptest](
 	[value] [varchar](50) NULL,
 	[latest] [smallint] NOT NULL,
 	[created] [datetime] NULL
-) ON [MAIN]
+) ON [PRIMARY]
 
 CREATE TABLE [dbo].[sptestexternal](
 	[key] [bigint] IDENTITY(1,1) NOT NULL,
 	[type] [int] NOT NULL,
 	[va] [varchar](50) NOT NULL,
 	[owner] [bigint] NOT NULL
-) ON [MAIN]
+) ON [PRIMARY]
 
 CREATE TABLE [dbo].[sptesthierachical](
 	[key] [bigint] IDENTITY(1,1) NOT NULL,
 	[node] [varchar](50) NOT NULL,
 	[parent] [varchar](50) NULL,
 	[child] [varchar](50) NULL
-) ON [MAIN]
+) ON [PRIMARY]
 GO
 
+SET IDENTITY_INSERT [dbo].[sptest] ON 
+GO
+INSERT [dbo].[sptest] ([primaryKey], [id], [type], [value], [latest], [created]) VALUES (2, 1, 1, N'1', 1, NULL)
+GO
+INSERT [dbo].[sptest] ([primaryKey], [id], [type], [value], [latest], [created]) VALUES (3, 2, 1, N'2', 0, NULL)
+GO
+INSERT [dbo].[sptest] ([primaryKey], [id], [type], [value], [latest], [created]) VALUES (4, 2, 1, N'3', 1, NULL)
+GO
+INSERT [dbo].[sptest] ([primaryKey], [id], [type], [value], [latest], [created]) VALUES (5, 3, 2, N'6', 1, NULL)
+GO
+SET IDENTITY_INSERT [dbo].[sptest] OFF
+GO
+SET IDENTITY_INSERT [dbo].[sptestexternal] ON 
+GO
+INSERT [dbo].[sptestexternal] ([key], [type], [va], [owner]) VALUES (3, 10, N'33', 2)
+GO
+INSERT [dbo].[sptestexternal] ([key], [type], [va], [owner]) VALUES (4, 11, N'55', 3)
+GO
+INSERT [dbo].[sptestexternal] ([key], [type], [va], [owner]) VALUES (5, 14, N'66', 4)
+GO
+INSERT [dbo].[sptestexternal] ([key], [type], [va], [owner]) VALUES (6, 12, N'77', 5)
+GO
+SET IDENTITY_INSERT [dbo].[sptestexternal] OFF
+GO
+SET IDENTITY_INSERT [dbo].[sptesthierachical] ON 
+GO
+INSERT [dbo].[sptesthierachical] ([key], [node], [parent], [child]) VALUES (1, N'2', NULL, NULL)
+GO
+INSERT [dbo].[sptesthierachical] ([key], [node], [parent], [child]) VALUES (2, N'3', N'2', NULL)
+GO
+INSERT [dbo].[sptesthierachical] ([key], [node], [parent], [child]) VALUES (3, N'4', N'2', NULL)
+GO
+INSERT [dbo].[sptesthierachical] ([key], [node], [parent], [child]) VALUES (4, N'5', N'4', NULL)
+GO
+SET IDENTITY_INSERT [dbo].[sptesthierachical] OFF
+GO
 
